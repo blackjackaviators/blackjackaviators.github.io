3e9c98a09b6c20a6fce8cd1cd054491e4bc90256
Michael Wright <mkwright@gmail.com>
2016-05-13 11:35:41 -0600

    Adjusted the source to be elm till angular 2 is faster at rendering

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class BlackjackAviatorsPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('blackjack-aviators-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {BlackjackAviatorsAppComponent} from './blackjack-aviators.component';
