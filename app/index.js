226f90849b4c70cf05e9b41d4e2f5601c601890b
Michael Wright <mkwright@gmail.com>
2016-05-13 11:39:01 -0600

    Automatic deploy build

--- app/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var environment_1 = require('./environment');
-exports.environment = environment_1.environment;
-var blackjack_aviators_component_1 = require('./blackjack-aviators.component');
-exports.BlackjackAviatorsAppComponent = blackjack_aviators_component_1.BlackjackAviatorsAppComponent;
-//# sourceMappingURL=index.js.map
